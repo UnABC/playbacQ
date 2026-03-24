b4258bab1e764bb1a4104128c45e6bdf683949da
UnABC <xiaotudou888@gmail.com>
2026-03-24 11:54:32 +0000

    CORSのフォールバック処理を追加

--- filters/AuthFilter.cpp
+++ filters/AuthFilter.cpp
@@ -8,9 +8,9 @@
 
 using namespace drogon;
 
-void AuthFilter::doFilter(const HttpRequestPtr &req,
-                         FilterCallback &&fcb,
-                         FilterChainCallback &&fccb)
+void AuthFilter::doFilter(const HttpRequestPtr& req,
+    FilterCallback&& fcb,
+    FilterChainCallback&& fccb)
 {
     std::string userId = req->getHeader("x-forwarded-user");
     if (!userId.empty())
@@ -23,5 +23,17 @@ void AuthFilter::doFilter(const HttpRequestPtr &req,
     auto res = drogon::HttpResponse::newHttpResponse();
     res->setStatusCode(k401Unauthorized);
     res->setBody("Unauthorized: X-Forwarded-User header is missing");
+
+    std::string origin = req->getHeader("Origin");
+    if (!origin.empty()) {
+        res->addHeader("Access-Control-Allow-Origin", origin);
+    } else {
+        // 念のためのフォールバック
+        auto customConfig = drogon::app().getCustomConfig();
+        res->addHeader("Access-Control-Allow-Origin", customConfig["baseUrl"].asString());
+    }
+    res->addHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
+    res->addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
     fcb(res);
 }
--- filters/AuthFilter.h
+++ filters/AuthFilter.h
@@ -7,15 +7,16 @@
 #pragma once
 
 #include <drogon/HttpFilter.h>
+#include <drogon/drogon.h>
 using namespace drogon;
 
 
 class AuthFilter : public HttpFilter<AuthFilter>
 {
-  public:
-    AuthFilter() {}
-    void doFilter(const HttpRequestPtr &req,
-                  FilterCallback &&fcb,
-                  FilterChainCallback &&fccb) override;
+public:
+  AuthFilter() {}
+  void doFilter(const HttpRequestPtr& req,
+    FilterCallback&& fcb,
+    FilterChainCallback&& fccb) override;
 };
 
