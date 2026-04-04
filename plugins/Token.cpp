1e69e6262970735c530c7b6738ce0fddf75f9508
UnABC <xiaotudou888@gmail.com>
2026-04-04 19:47:39 +0000

    埋め込み用トークン生成ロジックを簡素化し、トークンの有効期限を削除

--- plugins/Token.cpp
+++ plugins/Token.cpp
@@ -4,52 +4,23 @@ namespace Token {
 
 	std::string generateEmbedToken(const std::string& videoId)
 	{
-		auto expiry = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now() + std::chrono::hours(24));
-		std::string message = std::format("{0}:{1}", videoId, expiry);
-
 		unsigned char hash[EVP_MAX_MD_SIZE];
 		unsigned int hashLen = 0;
 		const char* EMBED_TOKEN_SECRET_KEY = std::getenv("EMBED_TOKEN_SECRET_KEY");
 		std::string SECRET_KEY = EMBED_TOKEN_SECRET_KEY ? EMBED_TOKEN_SECRET_KEY : "default_secret_key";
 		HMAC(EVP_sha256(), SECRET_KEY.data(), SECRET_KEY.size(),
-			reinterpret_cast<const unsigned char*>(message.data()), message.size(),
+			reinterpret_cast<const unsigned char*>(videoId.data()), videoId.size(),
 			hash, &hashLen);
 
 		std::string signature;
 		for (unsigned int i = 0;i < hashLen;++i) {
 			signature += std::format("{:02x}", hash[i]);
 		}
-		return std::format("{0}.{1}", expiry, signature);
+		return signature;
 	}
 
 	bool validateToken(const std::string& videoId, const std::string& token)
 	{
-		auto parts = token.find('.');
-		if (parts == std::string::npos) {
-			return false;
-		}
-		std::string expiryStr = token.substr(0, parts);
-		std::string signature = token.substr(parts + 1);
-
-		time_t expiry = std::stoll(expiryStr);
-		if (std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()) > expiry) {
-			return false;
-		}
-
-		std::string message = std::format("{0}:{1}", videoId, expiry);
-
-		unsigned char hash[EVP_MAX_MD_SIZE];
-		unsigned int hashLen = 0;
-		const char* EMBED_TOKEN_SECRET_KEY = std::getenv("EMBED_TOKEN_SECRET_KEY");
-		std::string SECRET_KEY = EMBED_TOKEN_SECRET_KEY ? EMBED_TOKEN_SECRET_KEY : "default_secret_key";
-		HMAC(EVP_sha256(), SECRET_KEY.data(), SECRET_KEY.size(),
-			reinterpret_cast<const unsigned char*>(message.data()), message.size(),
-			hash, &hashLen);
-
-		std::string expectedSignature;
-		for (unsigned int i = 0;i < hashLen;++i) {
-			expectedSignature += std::format("{:02x}", hash[i]);
-		}
-		return expectedSignature == signature;
+		return generateEmbedToken(videoId) == token;
 	}
 }
